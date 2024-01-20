163b44dc66bf716b963809900c100b9337db2279
Dongjun Yang <motokrzr11@gmail.com>
2024-01-20 16:59:33 +0900

    feat : header 추가 및 side 메뉴 디테일 수정

--- src/index.js
+++ src/index.js
@@ -1,2 +1,37 @@
 /* git config --global core.autocrlf true */
 /* npx tailwindcss -i ./src/input.css -o ./src/output.css --watch */ 
+
+/* 메뉴 hover 이벤트 */
+const alink = document.querySelectorAll('nav ul li');
+    
+alink.forEach(element => {
+    element.addEventListener('mouseover', function(e){
+        element.childNodes[3].style.display = "block";
+    });
+    element.addEventListener('mouseout', function(e){
+        element.childNodes[3].style.display = "none";
+    });
+});
+
+// 범위 이탈 및 접근시
+const content = document.querySelector("#mainVisual");
+
+let options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0,
+};
+
+let callback = (entries, observer) => {
+    entries.forEach((entry) => {
+        if (entry.intersectionRatio > 0) {
+            entry.target.classList.remove("style-show");
+        } else {
+            entry.target.classList.add("style-show");
+        }
+    });
+};
+
+let observer = new IntersectionObserver(callback, options);
+
+observer.observe(content)
