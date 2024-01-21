4cab647466f347c996b3dd7f4f08c973d3344456
Dongjun Yang <motokrzr11@gmail.com>
2024-01-21 14:07:00 +0900

    feat : 즉시 실행 함수 추가

--- src/index.js
+++ src/index.js
@@ -1,37 +1,41 @@
 /* git config --global core.autocrlf true */
 /* npx tailwindcss -i ./src/input.css -o ./src/output.css --watch */ 
 
-/* 메뉴 hover 이벤트 */
-const alink = document.querySelectorAll('nav ul li');
+/* 메뉴 드롭 다운 */
+(() => {
+    const alink = document.querySelectorAll('nav ul li');
     
-alink.forEach(element => {
-    element.addEventListener('mouseover', function(e){
-        element.childNodes[3].style.display = "block";
+    alink.forEach(element => {
+        element.addEventListener('mouseover', function(e){
+            element.childNodes[3].style.display = "block";
+        });
+        element.addEventListener('mouseout', function(e){
+            element.childNodes[3].style.display = "none";
+        });
     });
-    element.addEventListener('mouseout', function(e){
-        element.childNodes[3].style.display = "none";
-    });
-});
+})();
 
-// 범위 이탈 및 접근시
-const content = document.querySelector("#mainVisual");
+/* 스크롤 헤더 고정 */
+(() => {
+    const content = document.querySelector("#mainVisual");
 
-let options = {
-    root: null,
-    rootMargin: "0px",
-    threshold: 0,
-};
+    let options = {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0,
+    };
 
-let callback = (entries, observer) => {
-    entries.forEach((entry) => {
-        if (entry.intersectionRatio > 0) {
-            entry.target.classList.remove("style-show");
-        } else {
-            entry.target.classList.add("style-show");
-        }
-    });
-};
+    let callback = (entries, observer) => {
+        entries.forEach((entry) => {
+            if (entry.intersectionRatio > 0) {
+                entry.target.classList.remove("style-show");
+            } else {
+                entry.target.classList.add("style-show");
+            }
+        });
+    };
 
-let observer = new IntersectionObserver(callback, options);
+    let observer = new IntersectionObserver(callback, options);
 
-observer.observe(content)
+    observer.observe(content);
+})();
