51a158d538c4134602c9300bbd149acd5961ba08
dongjun <dongjun@hbedu.co.kr>
2024-01-24 17:22:28 +0900

    feat : 명언 새로고침 기능 추가

--- src/index.js
+++ src/index.js
@@ -41,14 +41,46 @@
 })();
 
 /* 명언 입력 */
-// (() => {
-//     const wiseSaying = [
-//         "꿈을 기록하는 것이 나의 목표였던 적은 없다, 꿈을 실현하는 것이 나의 목표이다.<br><br>- 만 레이<br>초현실주의와 다다이스트 사진가.",
-//         "인간은 오직 사고(思考)의 산물일 뿐이다. 생각하는 대로 되는 법.<br><br>- 마하트마 간디<br>인도의 정신적·정치적 지도자",
-//         "스스로를 존경하면 다른 사람도 당신을 존경할 것이다.<br><br>-공자<br>동아시아 인문주의의 원형이 된 고대 중국의 사상가",
-//         "여가시간을 가지려면 시간을 잘 써라.<br>-벤자민 프랭클린<br>미국의 정치인, 과학자, 저술가.",
-//     ]
+const wiseSayingObj = [
+    {
+        saying: '스스로를 존경하면 다른 사람도 당신을 존경할 것이다.',
+        human: '공자',
+        info: '동아시아 인문주의의 원형이 된 고대 중국의 사상가'
+    },
+    {
+        saying: '인간은 오직 사고(思考)의 산물일 뿐이다. 생각하는 대로 되는 법.',
+        human: '마하트마 간디',
+        info: '인도의 정신적·정치적 지도자'
+    },
+    {
+        saying: '스스로를 존경하면 다른 사람도 당신을 존경할 것이다.',
+        human: '공자',
+        info: '동아시아 인문주의의 원형이 된 고대 중국의 사상가'
+    },
+    {
+        saying: '여가시간을 가지려면 시간을 잘 써라.',
+        human: '벤자민 프랭클린',
+        info: '미국의 정치인, 과학자, 저술가.'
+    }
+]
 
-//     const wiseSayingElement = document.querySelector("#wiseSaying ");
-//     wiseSayingElement.innerHTML = wiseSaying[Math.floor(Math.random() * wiseSaying.length)]
-// })();
+const fnWiseSaying = (wiseSayingObj) => {
+    const wiseSayingEle = document.querySelector("#wiseSaying");
+    const wiseSayingHumanEle = document.querySelector("#wiseSayingHuman");
+    const wiseSayingHumanInfoEle = document.querySelector("#wiseSayingHumanInfo");
+
+    const wishSayingObjRandom = wiseSayingObj[Math.floor(Math.random() * wiseSayingObj.length)];
+    
+    wiseSayingEle.innerHTML = wishSayingObjRandom.saying;
+    wiseSayingHumanEle.innerHTML = wishSayingObjRandom.human;
+    wiseSayingHumanInfoEle.innerHTML = wishSayingObjRandom.info;
+}
+
+fnWiseSaying(wiseSayingObj);
+
+(() => {
+    const refreshBtn = document.querySelector("#refreshBtn");
+    refreshBtn.addEventListener('click', () => {
+        fnWiseSaying(wiseSayingObj);
+    })
+})();
