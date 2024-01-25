94f48126e463f6714cbf4e6a896767023568767e
Dongjun Yang <motokrzr11@gmail.com>
2024-01-25 22:33:57 +0900

    fix : 새로고침 중 명언 생성

--- src/index.js
+++ src/index.js
@@ -53,9 +53,9 @@ const wiseSayingObj = [
         info: '인도의 정신적·정치적 지도자'
     },
     {
-        saying: '스스로를 존경하면 다른 사람도 당신을 존경할 것이다.',
-        human: '공자',
-        info: '동아시아 인문주의의 원형이 된 고대 중국의 사상가'
+        saying: '능력이 부족할 수록 자만심이 더 강하다.',
+        human: '아하드 하암',
+        info: '잠이 오는구만'
     },
     {
         saying: '여가시간을 가지려면 시간을 잘 써라.',
@@ -64,32 +64,46 @@ const wiseSayingObj = [
     }
 ]
 
-let fnWiseSaying = (wiseSayingObj, ranNum) => {
-    const wiseSayingEle = document.querySelector("#wiseSaying");
-    const wiseSayingHumanEle = document.querySelector("#wiseSayingHuman");
-    const wiseSayingHumanInfoEle = document.querySelector("#wiseSayingHumanInfo");
+const wiseSayingEle = document.querySelector("#wiseSaying");
+const wiseSayingHumanEle = document.querySelector("#wiseSayingHuman");
+const wiseSayingHumanInfoEle = document.querySelector("#wiseSayingHumanInfo");
 
-    wiseSayingEle.innerHTML = wiseSayingObj[ranNum].saying;
-    wiseSayingHumanEle.innerHTML = wiseSayingObj[ranNum].human;
-    wiseSayingHumanInfoEle.innerHTML = wiseSayingObj[ranNum].info;
-}
-
-// localStorage.clear();
-let strogeRanNum = Math.floor(Math.random() * wiseSayingObj.length);
 let ranNum = Math.floor(Math.random() * wiseSayingObj.length);
-localStorage.setItem("localNumber", strogeRanNum);
 
-if(parseInt(localStorage.getItem("localNumber")) === parseInt(ranNum)){
-    let newRanNum = Math.floor(Math.random() * wiseSayingObj.length);
-    fnWiseSaying(wiseSayingObj, newRanNum);
-} else {
-    fnWiseSaying(wiseSayingObj, parseInt(localStorage.getItem("localNumber")));
-}
+wiseSayingEle.innerHTML = wiseSayingObj[ranNum].saying;
+wiseSayingHumanEle.innerHTML = wiseSayingObj[ranNum].human;
+wiseSayingHumanInfoEle.innerHTML = wiseSayingObj[ranNum].info;
 
-// (() => {
-//     const refreshBtn = document.querySelector("#refreshBtn");
-//     refreshBtn.addEventListener('click', () => {
-//         fnWiseSaying(wiseSayingObj);
-//     })
-// })();
-//const ranNum = wiseSayingObj[Math.floor(Math.random() * wiseSayingObj.length)];
+// 배열 복사 확인
+// const wiseSayingEle = document.querySelector("#wiseSaying");
+// const wiseSayingHumanEle = document.querySelector("#wiseSayingHuman");
+// const wiseSayingHumanInfoEle = document.querySelector("#wiseSayingHumanInfo");
+// let tries = [];
+
+// for(let n = 0; n < wiseSayingObj.length; n++){
+//     tries.push(n);
+// }
+// console.log(`시작 : ${tries.length}`);
+// let ranNum = Math.floor(Math.random() * tries.length);
+
+// wiseSayingEle.innerHTML = wiseSayingObj[ranNum].saying;
+// wiseSayingHumanEle.innerHTML = wiseSayingObj[ranNum].human;
+// wiseSayingHumanInfoEle.innerHTML = wiseSayingObj[ranNum].info;
+
+// tries.splice(ranNum, 1);
+// console.log(`끝 : ${tries.length}`);
+
+// refreshBtn.addEventListener("click", function(){
+//     console.log(`함수시작 : ${tries.length}`);
+//     let ranNum = Math.floor(Math.random() * tries.length);
+//     tries.splice(ranNum, 1);
+//     console.log(`함수끝 : ${tries.length}`);
+
+//     wiseSayingEle.innerHTML = wiseSayingObj[ranNum].saying;
+//     wiseSayingHumanEle.innerHTML = wiseSayingObj[ranNum].human;
+//     wiseSayingHumanInfoEle.innerHTML = wiseSayingObj[ranNum].info;
+
+//     if(tries.length < 1){
+//         console.log('초기화')
+//     }
+// });
