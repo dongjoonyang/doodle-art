a05535eec320dbd400cd3d7d5a419eca14153c89
Dongjun Yang <motokrzr11@gmail.com>
2024-01-26 19:25:40 +0900

    feat : 명언 코드 정리

--- src/index.js
+++ src/index.js
@@ -41,74 +41,72 @@
 })();
 
 /* 명언 입력 */
-const wiseSayingObj = [
-    {
-        saying: '1',
-        human: '공자',
-        info: '동아시아 인문주의의 원형이 된 고대 중국의 사상가'
-    },
-    {
-        saying: '2',
-        human: '마하트마 간디',
-        info: '인도의 정신적·정치적 지도자'
-    },
-    {
-        saying: '3',
-        human: '아하드 하암',
-        info: '잠이 오는구만'
-    },
-    {
-        saying: '4',
-        human: '벤자민 프랭클린',
-        info: '미국의 정치인, 과학자, 저술가.'
-    }
-]
+(() => {
+    const wiseSayingObj = [
+        {
+            saying: '1',
+            human: '공자',
+            info: '동아시아 인문주의의 원형이 된 고대 중국의 사상가'
+        },
+        {
+            saying: '2',
+            human: '마하트마 간디',
+            info: '인도의 정신적·정치적 지도자'
+        },
+        {
+            saying: '3',
+            human: '아하드 하암',
+            info: '잠이 오는구만'
+        },
+        {
+            saying: '4',
+            human: '벤자민 프랭클린',
+            info: '미국의 정치인, 과학자, 저술가.'
+        }
+    ]
 
-let wiseSayingObjClone = [];
-Object.assign(wiseSayingObjClone, wiseSayingObj);
+    let wiseSayingObjClone = [];
+    Object.assign(wiseSayingObjClone, wiseSayingObj);
 
-const wiseSayingEle = document.querySelector("#wiseSaying");
-const wiseSayingHumanEle = document.querySelector("#wiseSayingHuman");
-const wiseSayingHumanInfoEle = document.querySelector("#wiseSayingHumanInfo");
+    const wiseSayingEle = document.querySelector("#wiseSaying");
+    const wiseSayingHumanEle = document.querySelector("#wiseSayingHuman");
+    const wiseSayingHumanInfoEle = document.querySelector("#wiseSayingHumanInfo");
 
-let num = 0;
-wiseSayingEle.innerHTML = wiseSayingObjClone[num].saying;
-wiseSayingHumanEle.innerHTML = wiseSayingObjClone[num].human;
-wiseSayingHumanInfoEle.innerHTML = wiseSayingObjClone[num].info;
-
-wiseSayingObjClone.splice(num, 1);
-
-refreshBtn.addEventListener("click", function(){
     let num = 0;
-
-    if(wiseSayingObjClone.length < 1){
-        num = 0;
-        Object.assign(wiseSayingObjClone, wiseSayingObj);
-    }
-    
     wiseSayingEle.innerHTML = wiseSayingObjClone[num].saying;
     wiseSayingHumanEle.innerHTML = wiseSayingObjClone[num].human;
     wiseSayingHumanInfoEle.innerHTML = wiseSayingObjClone[num].info;
 
     wiseSayingObjClone.splice(num, 1);
-    console.dir(`${JSON.stringify(wiseSayingObjClone, null, 2)}`);
-    console.log(`${wiseSayingObjClone.length}`);
 
-    num += 1;
-});
+    refreshBtn.addEventListener("click", function(){
+        let num = 0;
 
-let fnInterval = () => {
-    let progressBar = document.querySelector("#progressbar");
-    progressBar.value = progressBar.value + 1;
-    
-    if(progressBar.value === 100){
-        progressBar.value = 0;
+        if(wiseSayingObjClone.length < 1){
+            num = 0;
+            Object.assign(wiseSayingObjClone, wiseSayingObj);
+        }
+        
+        wiseSayingEle.innerHTML = wiseSayingObjClone[num].saying;
+        wiseSayingHumanEle.innerHTML = wiseSayingObjClone[num].human;
+        wiseSayingHumanInfoEle.innerHTML = wiseSayingObjClone[num].info;
+
+        wiseSayingObjClone.splice(num, 1);
+
+        num += 1;
+    });
+    let fnInterval = () => {
+        let progressBar = document.querySelector("#progressbar");
+        if(progressBar.value === 100){
+            progressBar.value = 0;
+        }
+        progressBar.value = progressBar.value + 1;
     }
-}
 
-let interval = setInterval(fnInterval, 50);
+    let interval = setInterval(fnInterval, 50);
 
-let timerId = setTimeout(function refreshTrigger() {
-    refreshBtn.click();
-    timerId = setTimeout(refreshTrigger, 5000);
-  }, 5000);
+    let fnTimer = setTimeout(function refreshTrigger() {
+        refreshBtn.click();
+        fnTimer = setTimeout(refreshTrigger, 5000);
+    }, 5000);
+})();
